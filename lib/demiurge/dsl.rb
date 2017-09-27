b3e7d10cf833d27d2b5fe9e9d3e9db6fc16fd4d7
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-09-27 13:50:55 -0700

    Add more actions, start out toward running DSL actions properly...

--- lib/demiurge/dsl.rb
+++ lib/demiurge/dsl.rb
@@ -30,12 +30,15 @@ module Demiurge
     def initialize(name)
       @name = name
       @locations = []
+      @actions = {}
     end
 
     def location(name, &block)
       builder = LocationBuilder.new(name)
       builder.instance_eval(&block)
-      @locations << builder.built_location
+      location = builder.built_location
+      @locations << location
+      DslLocation.register_actions_by_item_and_action_name(location[1] => builder.actions)
       nil
     end
 
@@ -84,6 +87,10 @@ module Demiurge
       super
     end
 
+    def state
+      @engine.state_for_item(@name)
+    end
+
     def self.register_actions_by_item_and_action_name(act)
       @actions ||= {}
       act.each do |item_name, act_hash|
@@ -104,7 +111,66 @@ module Demiurge
     def intentions_for_next_step(options = {})
       everies = @engine.state_for_property(@name, "everies")
       return [] if everies.empty?
-      intention = EveryXTicksIntention.new(@name)
+      EveryXTicksIntention.new(@name)
+    end
+
+    def run_action(action_name)
+      block = DslLocation.action_for_item(@name, action_name)
+      raise "No such action as #{action_name.inspect} for #{@name.inspect}!" unless block
+      @block_runner ||= DslLocationBlockRunner.new(self)
+      @block_runner.instance_eval(&block)
+      nil
+    end
+  end
+
+  class DslLocationBlockRunner
+    def initialize(location)
+      @location = location
+    end
+
+    def state
+      @state_wrapper ||= DslLocationStateWrapper.new(@location)
+    end
+
+    def action(*args)
+      STDERR.puts "Not yet using action: #{args.inspect}"
+    end
+  end
+
+  class DslLocationStateWrapper
+    def initialize(location)
+      @location = location
+    end
+
+    def method_missing(method_name, *args, &block)
+      if method_name.to_s[-1] == "="
+        getter_name = method_name.to_s[0..-2]
+        setter_name = method_name.to_s
+      else
+        getter_name = method_name.to_s
+        setter_name = method_name.to_s + "="
+      end
+
+      STDERR.puts "Method missing: #{method_name.inspect} / #{getter_name.inspect} / #{setter_name.inspect} / #{@location.state.inspect}"
+      if @location.state.has_key?(getter_name)
+        self.class.define_method(getter_name) do
+          @location.state[getter_name]
+        end
+        self.class.define_method(setter_name) do |val|
+          @location.state[setter_name] = val
+        end
+
+        # Call to new defined method
+        return self.send(method_name, *args, &block)
+      end
+
+      # Nope, no matching state.
+      STDERR.puts "No such state key as #{method_name.inspect} in DslLocationStateWrapper#method_missing!"
+      super
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      @location.state.has_key?(method_name.to_s) || super
     end
   end
 
@@ -120,10 +186,12 @@ module Demiurge
     def apply(engine, options)
       everies = engine.state_for_property(@name, "everies")
       everies.each do |every|
-        every["counter"] += 1  # TODO: Use set_state_for_property?
+        every["counter"] += 1
         if every["counter"] >= every["every"]
-          STDERR.puts "Time to execute action #{every["action"]}!"
-          every["counter"] = 0 # TODO: use set_state_for_property?
+          STDERR.puts "Time to execute action #{every["action"].inspect} on item #{@name.inspect} (every #{every["every"]} ticks)!"
+          item = engine.item_by_name(@name)
+          item.run_action(every["action"])
+          every["counter"] = 0
         end
       end
     end
