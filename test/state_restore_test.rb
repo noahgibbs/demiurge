67cf5cfd847802ac1a1ea975a0c636225f91bc22
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-12-21 15:12:48 -0800

    Test intentions and notifications across state load/restore pretty aggressively.

--- test/state_restore_test.rb
+++ test/state_restore_test.rb
@@ -6,7 +6,7 @@ class ZoneSubtype < Demiurge::Zone; end
 Demiurge::DSL::TopLevelBuilder.register_type "ZoneSubtype", ZoneSubtype
 
 class StateRestoreTest < Minitest::Test
-  DSL_TEXT = <<-GOBLIN_DSL
+  DSL_TEXT = <<-DSL
     zone "moss caves" do
       location "first moss cave" do
         description "This cave is dim, with smooth sides. You can see delicious moss growing inside, out of the hot sunlight."
@@ -22,6 +22,8 @@ class StateRestoreTest < Minitest::Test
         description "A recently-opened cave here has jagged walls, and delicious-looking stubbly moss in between the cracks."
         state.moss = 0
         state.softer_moss = 0
+        state.no_flash = true
+        state.messages = []
 
         every_X_ticks("grow", 3) do
           state.moss += 1
@@ -30,14 +32,39 @@ class StateRestoreTest < Minitest::Test
         end
 
         agent "wanderer", "type" => "WanderingAgent" do
-          # Don't declare a location - this should get one automatically.
+        end
+
+        on_intention("flash of light", "see the flash") do
+          state.saw_maybe_flash = true
+          if state.no_flash
+            cancel_intention "This room is in no way groovy enough."
+          end
+        end
+
+        on_notification("sight", "check the sights") do |n|
+          state.messages.push(n["message"])
+          if n["mirrored"]
+            state.totally_funky = true
+          end
+        end
+
+        agent "disco bandit" do
+          state.mirrored = false
+
+          define_action("flash of light") do
+            if state.mirrored
+              notification "type": "sight", "message": "A bright double burst of light fills the room!", "mirrored": true
+            else
+              notification "type": "sight", "message": "A bright burst of light fills the room!"
+            end
+          end
         end
       end
     end
-  GOBLIN_DSL
+  DSL
 
   def test_dsl_actions_after_state_restore
-    engine = Demiurge.engine_from_dsl_text(["Goblin DSL", DSL_TEXT])
+    engine = Demiurge.engine_from_dsl_text(["State Restore DSL", DSL_TEXT])
     first_cave_item = engine.item_by_name("first moss cave")
     refute_nil first_cave_item
     second_cave_item = engine.item_by_name("second moss cave")
@@ -71,9 +98,6 @@ class StateRestoreTest < Minitest::Test
 
     assert_equal 0, first_cave_item.state["moss"]
     assert_equal 0, second_cave_item.state["moss"]
-    # We won't apply these intentions, they get calculated again when advancing a tick.
-    intentions = engine.next_step_intentions
-    assert_equal 5, intentions.size  # Two from the moss caves, three from the agent
 
     engine.advance_one_tick
     assert_equal 0, first_cave_item.state["moss"]
@@ -89,7 +113,7 @@ class StateRestoreTest < Minitest::Test
   end
 
   def test_dsl_actions_with_middle_state_restore
-    engine = Demiurge.engine_from_dsl_text(["Goblin DSL", DSL_TEXT])
+    engine = Demiurge.engine_from_dsl_text(["State Restore DSL", DSL_TEXT])
     first_cave_item = engine.item_by_name("first moss cave")
     refute_nil first_cave_item
     second_cave_item = engine.item_by_name("second moss cave")
@@ -100,8 +124,6 @@ class StateRestoreTest < Minitest::Test
 
     assert_equal 0, first_cave_item.state["moss"]
     assert_equal 0, second_cave_item.state["moss"]
-    intentions = engine.next_step_intentions
-    assert_equal 5, intentions.size  # Two from the moss caves, three from the agent
 
     # Dump and restore in the middle of all this...
     ss = engine.structured_state
@@ -119,12 +141,61 @@ class StateRestoreTest < Minitest::Test
     assert_equal 7, second_cave_item.state["softer_moss"]
   end
 
-  def test_dsl_type_specs
-    engine = Demiurge.engine_from_dsl_text(["Goblin DSL", <<-DSL_TEXT])
-zone "first zone", "type" => "ZoneSubtype" do
-end
-    DSL_TEXT
-    zone = engine.item_by_name("first zone")
-    assert_equal "ZoneSubtype", zone.class.to_s
+  def test_intentions_and_notifications_across_state_restore
+    engine = Demiurge.engine_from_dsl_text(["State Restore DSL", DSL_TEXT])
+    disco_cave = engine.item_by_name("second moss cave")
+    bandit = engine.item_by_name("disco bandit")
+
+    # Dump and restore to check if on_intention and on_notification keep working
+    ss = engine.structured_state
+    engine.load_state_from_dump(ss)
+
+    notifications = []
+    engine.subscribe_to_notifications("type": ["intention_cancelled", "sight"]) do |n|
+      notifications.push(n)
+    end
+
+    # Dump and restore to check if subscribe_to_notifications keeps working
+    ss = engine.structured_state
+    engine.load_state_from_dump(ss)
+
+    assert !disco_cave.state["saw_maybe_flash"], "The cave shouldn't have seen any flash yet!"
+    assert notifications.size == 0, "There shouldn't be any notifications since we just started!"
+    bandit.queue_action("flash of light")  # Right now, the cave is set "no_flash" and will cancel it.
+    # Dump and restore to check if the queued action still happens...
+    ss = engine.structured_state
+    engine.load_state_from_dump(ss)
+    # Done w/ restore, now go forward a tick
+    engine.advance_one_tick
+    assert_equal "intention_cancelled", notifications[0]["type"]
+    assert disco_cave.state["saw_maybe_flash"] == true, "The cave should have seen a flash and cancelled the action!"
+    notifications.pop  # Clear the queue
+
+    # Dump and restore, just in case
+    ss = engine.structured_state
+    engine.load_state_from_dump(ss)
+
+    disco_cave.state["no_flash"] = false  # Now the flash can happen, the cave won't cancel it
+    bandit.queue_action("flash of light")
+    engine.advance_one_tick
+    assert_equal 1, notifications.size
+    assert_equal "sight", notifications[0]["type"]
+    notifications.pop
+
+    # Dump and restore to check if everything still works
+    ss = engine.structured_state
+    engine.load_state_from_dump(ss)
+
+    bandit.state["mirrored"] = true
+    bandit.queue_action("flash of light")
+    # Dump and restore to check if the queued action still happens...
+    ss = engine.structured_state
+    engine.load_state_from_dump(ss)
+    # Done w/ restore, now go forward a tick
+    engine.advance_one_tick
+    assert_equal 1, notifications.size
+    assert_equal "sight", notifications[0]["type"]
+    assert disco_cave.state["totally_funky"] == true, "The cave didn't get totally funky! Something is wrong!"
+    notifications.pop
   end
 end
