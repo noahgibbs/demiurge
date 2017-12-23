70859b2a4700ea1e52148e6691a5d27056daa105
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-12-22 23:07:09 -0800

    Test all the same kinds of stability as for a state restore, but for world-code reload

--- test/world_reload_test.rb
+++ test/world_reload_test.rb
@@ -154,90 +154,90 @@ class WorldReloadTest < Minitest::Test
     assert_equal 7, second_cave_item.state["softer_moss"]
   end
 
-  #def test_dsl_actions_with_middle_world_restore
-  #  engine = Demiurge::DSL.engine_from_dsl_text(["World Reload DSL", DSL_TEXT])
-  #  first_cave_item = engine.item_by_name("first moss cave")
-  #  refute_nil first_cave_item
-  #  second_cave_item = engine.item_by_name("second moss cave")
-  #  refute_nil second_cave_item
-  #  agent = engine.item_by_name("wanderer")
-  #  refute_nil agent
-  #  assert_equal "second moss cave", agent.location_name
-  #
-  #  assert_equal 0, first_cave_item.state["moss"]
-  #  assert_equal 0, second_cave_item.state["moss"]
-  #
-  #  # Dump and restore in the middle of all this...
-  #  ss = engine.structured_state
-  #  engine.load_state_from_dump(ss)
-  #
-  #  engine.advance_one_tick
-  #  assert_equal 0, first_cave_item.state["moss"]
-  #  assert_equal 0, second_cave_item.state["moss"]
-  #
-  #  engine.advance_one_tick
-  #  engine.advance_one_tick
-  #  assert_equal 1, first_cave_item.state["moss"]
-  #  assert_nil first_cave_item.state["softer_moss"]
-  #  assert_equal 1, second_cave_item.state["moss"]
-  #  assert_equal 7, second_cave_item.state["softer_moss"]
-  #end
+  def test_dsl_actions_with_middle_world_restore
+    engine = Demiurge::DSL.engine_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    first_cave_item = engine.item_by_name("first moss cave")
+    refute_nil first_cave_item
+    second_cave_item = engine.item_by_name("second moss cave")
+    refute_nil second_cave_item
+    agent = engine.item_by_name("wanderer")
+    refute_nil agent
+    assert_equal "second moss cave", agent.location_name
 
-  #def test_intentions_and_notifications_across_world_restore
-  #  engine = Demiurge::DSL.engine_from_dsl_text(["World Reload DSL", DSL_TEXT])
-  #  disco_cave = engine.item_by_name("second moss cave")
-  #  bandit = engine.item_by_name("disco bandit")
-  #
-  #  # Dump and restore to check if on_intention and on_notification keep working
-  #  ss = engine.structured_state
-  #  engine.load_state_from_dump(ss)
-  #
-  #  notifications = []
-  #  engine.subscribe_to_notifications("type": ["intention_cancelled", "sight"]) do |n|
-  #    notifications.push(n)
-  #  end
-  #
-  #  # Dump and restore to check if subscribe_to_notifications keeps working
-  #  ss = engine.structured_state
-  #  engine.load_state_from_dump(ss)
-  #
-  #  assert !disco_cave.state["saw_maybe_flash"], "The cave shouldn't have seen any flash yet!"
-  #  assert notifications.size == 0, "There shouldn't be any notifications since we just started!"
-  #  bandit.queue_action("flash of light")  # Right now, the cave is set "no_flash" and will cancel it.
-  #  # Dump and restore to check if the queued action still happens...
-  #  ss = engine.structured_state
-  #  engine.load_state_from_dump(ss)
-  #  # Done w/ restore, now go forward a tick
-  #  engine.advance_one_tick
-  #  assert_equal "intention_cancelled", notifications[0]["type"]
-  #  assert disco_cave.state["saw_maybe_flash"] == true, "The cave should have seen a flash and cancelled the action!"
-  #  notifications.pop  # Clear the queue
-  #
-  #  # Dump and restore, just in case
-  #  ss = engine.structured_state
-  #  engine.load_state_from_dump(ss)
-  #
-  #  disco_cave.state["no_flash"] = false  # Now the flash can happen, the cave won't cancel it
-  #  bandit.queue_action("flash of light")
-  #  engine.advance_one_tick
-  #  assert_equal 1, notifications.size
-  #  assert_equal "sight", notifications[0]["type"]
-  #  notifications.pop
-  #
-  #  # Dump and restore to check if everything still works
-  #  ss = engine.structured_state
-  #  engine.load_state_from_dump(ss)
-  #
-  #  bandit.state["mirrored"] = true
-  #  bandit.queue_action("flash of light")
-  #  # Dump and restore to check if the queued action still happens...
-  #  ss = engine.structured_state
-  #  engine.load_state_from_dump(ss)
-  #  # Done w/ restore, now go forward a tick
-  #  engine.advance_one_tick
-  #  assert_equal 1, notifications.size
-  #  assert_equal "sight", notifications[0]["type"]
-  #  assert disco_cave.state["totally_funky"] == true, "The cave didn't get totally funky! Something is wrong!"
-  #  notifications.pop
-  #end
+    assert_equal 0, first_cave_item.state["moss"]
+    assert_equal 0, second_cave_item.state["moss"]
+
+    # Reload in the middle of all this...
+    engine.reload_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    engine.flush_notifications
+
+    engine.advance_one_tick
+    assert_equal 0, first_cave_item.state["moss"]
+    assert_equal 0, second_cave_item.state["moss"]
+
+    engine.advance_one_tick
+    engine.advance_one_tick
+    assert_equal 1, first_cave_item.state["moss"]
+    assert_nil first_cave_item.state["softer_moss"]
+    assert_equal 1, second_cave_item.state["moss"]
+    assert_equal 7, second_cave_item.state["softer_moss"]
+  end
+
+  def test_intentions_and_notifications_across_world_restore
+    engine = Demiurge::DSL.engine_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    disco_cave = engine.item_by_name("second moss cave")
+    bandit = engine.item_by_name("disco bandit")
+
+    # Reload to check if on_intention and on_notification keep working
+    engine.reload_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    engine.flush_notifications
+
+    notifications = []
+    engine.subscribe_to_notifications("type": ["intention_cancelled", "sight"]) do |n|
+      notifications.push(n)
+    end
+
+    # Reload to check if subscribe_to_notifications keeps working
+    engine.reload_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    engine.flush_notifications
+
+    assert !disco_cave.state["saw_maybe_flash"], "The cave shouldn't have seen any flash yet!"
+    assert notifications.size == 0, "There shouldn't be any notifications since we just started!"
+    bandit.queue_action("flash of light")  # Right now, the cave is set "no_flash" and will cancel it.
+    # Reload to check if the queued action still happens...
+    engine.reload_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    engine.flush_notifications
+    # Done w/ restore, now go forward a tick
+    engine.advance_one_tick
+    assert_equal "intention_cancelled", notifications[0]["type"]
+    assert disco_cave.state["saw_maybe_flash"] == true, "The cave should have seen a flash and cancelled the action!"
+    notifications.pop  # Clear the queue
+
+    # Reload just in case
+    engine.reload_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    engine.flush_notifications
+
+    disco_cave.state["no_flash"] = false  # Now the flash can happen, the cave won't cancel it
+    bandit.queue_action("flash of light")
+    engine.advance_one_tick
+    assert_equal 1, notifications.size
+    assert_equal "sight", notifications[0]["type"]
+    notifications.pop
+
+    # Reload to check if everything still works
+    engine.reload_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    engine.flush_notifications
+
+    bandit.state["mirrored"] = true
+    bandit.queue_action("flash of light")
+    # Reload to check if the queued action still happens...
+    engine.reload_from_dsl_text(["World Reload DSL", DSL_TEXT])
+    engine.flush_notifications
+    # Done w/ restore, now go forward a tick
+    engine.advance_one_tick
+    assert_equal 1, notifications.size
+    assert_equal "sight", notifications[0]["type"]
+    assert disco_cave.state["totally_funky"] == true, "The cave didn't get totally funky! Something is wrong!"
+    notifications.pop
+  end
 end
